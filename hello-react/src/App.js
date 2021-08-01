b708b4f3b2caa798d072b14da289e583136a5d59
김태형 <thk12378905@gmail.com>
2021-08-01 10:30:03 +0000

    event_handling_1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Say from './Say';
+import EventPractice from './EventPractice';
 
 const App =() => {
-	return <Say />;
+	return <EventPractice />;
 };
 export default App;
