9c517d6b1729d12049623ec50a2e4a70ed16cdd3
김태형 <thk12378905@gmail.com>
2021-08-01 07:56:13 +0000

    props

---
+++ hello-react/src/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import MyComponent from './MyComponent';
+
+const App =() => {
+	return <MyComponent name="react" favoriteNumber = {1}>리웩트!</MyComponent>;
+};
+export default App;
---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+const MyComponent = ({ name, favoriteNumber, children }) =>{
+	return (
+		<div>
+			제 이름은 {name}입니다.<br/>
+			children 값은 {children}
+			입니다.
+			<br />
+			제가 좋아하는 숫자는 {favoriteNumber}입니다.
+		</div>
+		);
+};
+
+MyComponent.defaultProps = {
+	name: '김태형'
+};
+
+MyComponent.propTypes = {
+	name: PropTypes.string,
+	favoriteNumber: PropTypes.number.isRequired
+};
+
+export default MyComponent;
