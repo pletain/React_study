eaca18021fd81d55a419f7bd440f961615b2ee1d
김태형 <thk12378905@gmail.com>
2021-08-05 11:10:09 +0000

    component Iteration

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -7,16 +7,33 @@ const IterationSample = () => {
 		{ id: 3, text: '눈'},
 		{ id: 4, text: '바람'},
 	]);
-	const [imputText, setInputText] = useState('');
+	const [inputText, setInputText] = useState('');
 	const [nextId, setNextId] = useState(5);
 	
 	const onChange = e => setInputText(e.target.value);
+	const onClick = () => {
+		const nextNames = names.concat({
+			id: nextId,
+			text: inputText
+		});
+		setNextId(nextId+1);
+		setNames(nextNames);
+		setInputText('');
+	};
+	const onRemove = id => {
+		const nextNames = names.filter(name => name.id !== id);
+		setNames(nextNames);
+	};
 	
-	const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+	const namesList = names.map(name => (
+		<li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+			{name.text}
+		</li>
+	));
 	return (
 		<>
 			<input value={inputText} onChange={onChange} />
-			<button>추가</button>
+			<button onClick={onClick}>추가</button>
 			<ul>{namesList}</ul>
 		</>
 	);
