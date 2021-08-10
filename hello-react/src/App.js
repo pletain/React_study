5ed31e0990702a12cc8829cc60b3bb2cfd2a22fd
김태형 <thk12378905@gmail.com>
2021-08-10 10:17:58 +0000

    lifecycle_errorBoundary

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import LifeCycleSample from './LifeCycleSample';
+import LifeCycleSample from './LifeCycle_catch';
+import ErrorBoundary from './ErrorBoundary'
 
 function getRandomColor(){
 	return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -20,7 +21,10 @@ class App extends Component {
 		return (
 		<div>
 			<button onClick={this.handleClick}>랜덤 색상</button>
-			<LifeCycleSample color={this.state.color}/>
+				<ErrorBoundary>
+					<LifeCycleSample color={this.state.color}/>
+				</ErrorBoundary>
+			
 		</div>
 		);
 	}
