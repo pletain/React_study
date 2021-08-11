6d68c7d6cee1f8e0eaf547d9e154c2fbed63de9b
김태형 <thk12378905@gmail.com>
2021-08-11 11:00:58 +0000

    useEffect

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Info from './Hooks_8/Info';
 
 const App = () => {
-	return <Info />;
+	const [visible, setVisible] = useState(false);
+	return (
+	<div>
+		<button
+			onClick={() => {
+					setVisible(!visible);
+				}}
+			>
+			{visible ? '숨기기' : '보이기'}
+			</button>
+		<hr />
+			{visible && <Info />}
+	</div>
+	);
 };
 
 export default App;
--- hello-react/src/Hooks_8/Info.js
+++ hello-react/src/Hooks_8/Info.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
 	const [name, setName] = useState(''); 
 	const [nickname,setNickname] = useState('');
+	useEffect(()=> { //컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정하는 Hook
+		console.log('렌더링이 완료되었습니다!');
+		console.log({
+			name, 
+			nickname
+		});
+		return () => {
+			console.log('clean up');
+			console.log(name);
+		}
+	}, []);
 	
 	const onChangeName = e => {
 		setName(e.target.value);
