1d1cb3816dadcdae031deb523d585ec939d7f4c6
김태형 <thk12378905@gmail.com>
2021-08-14 07:14:22 +0000

    useCallback

--- hello-react/src/Hooks_8/Average.js
+++ hello-react/src/Hooks_8/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
 const getAverage = numbers => {
 	console.log('평균값 계산 중...');
@@ -11,14 +11,14 @@ const Average = () => {
 	const [list, setList] = useState([]);
 	const [number, setNumber] = useState('');
 	
-	const onChange = e => {
+	const onChange = useCallback(e => {
 		setNumber(e.target.value);
-	};
-	const onInsert = e => {
+	}, []); //컴포넌트가 처음 렌더링 됐을 때만 함수 생성
+	const onInsert = useCallback( e => {
 		const nextList = list.concat(parseInt(number));
 		setList(nextList);
 		setNumber('');
-	};
+	}, [number, list]); //number 혹은 list만 바뀌었을 때만 함수 생성
 	
 	const avg = useMemo(()=>getAverage(list), [list]);
 	
