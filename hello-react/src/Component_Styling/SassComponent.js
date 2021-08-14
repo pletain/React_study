6b1f145e53436af0d70fd5882020d01c185635de
김태형 <thk12378905@gmail.com>
2021-08-14 13:31:08 +0000

    using sass

--- hello-react/src/Component_Styling/SassComponent.js
+++ hello-react/src/Component_Styling/SassComponent.js
@@ -4,9 +4,9 @@ import './SassComponent.scss';
 const SassComponent = () => {
 	return (
 	<div className="SassComponent">
-			<div className="box red" />
-			<div className="box orange" />
-			<div className="box green" />
+		<div className="box red" />
+		<div className="box orange" />
+		<div className="box green" />
 	</div>
 	)
 }
