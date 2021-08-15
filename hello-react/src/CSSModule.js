d6dbf371084729625956ef52e237e5540520f244
김태형 <thk12378905@gmail.com>
2021-08-15 00:58:39 +0000

    using several CSS Module

--- hello-react/src/CSSModule.js
+++ hello-react/src/CSSModule.js
@@ -3,7 +3,7 @@ import styles from './CSSModule.module.css';
 
 const CSSModule = () => {
 	return (
-	<div className={styles.wrapper}>
+	<div className={`${styles.wrapper} ${styles.inverted}`}>
 		안녕하세요 저는 <span className="something">CSS Module!</span>
 		</div>
 	);
