9594550a387868123be4659e6e940368ff0708c3
김태형 <thk12378905@gmail.com>
2021-08-15 04:43:52 +0000

    Styled_Component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,11 @@
 import React,  { Component } from 'react';
-import CSSModule from './CSSModule';
+import StyledComponent from './Component_Styling/StyledComponent';
 
 class App extends Component {
 	render() {
 	return (
 		<div>
-			<CSSModule />
+			<StyledComponent />
 		</div>
 	);
 	}
