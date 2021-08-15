254d251b14add9e733c35437eca63fc5d9ed5e77
김태형 <thk12378905@gmail.com>
2021-08-15 06:42:30 +0000

    Responsive Design

--- hello-react/src/Component_Styling/StyledComponent.js
+++ hello-react/src/Component_Styling/StyledComponent.js
@@ -5,6 +5,14 @@ const Box = styled.div`
 	background: ${props => props.color || 'blue'};
 	padding: 1rem;
 	display: flex;
+	width: 1024px;
+	margin: 0 auto;
+	@media (max-width: 1024px) {
+	width: 768px;
+	}
+	@media (max-width: 768px) {
+	width: 100%;
+	}
 `;
 
 const Button = styled.button`
