53a103c1cf5a04814530df569b91c5fecc61fcac
김태형 <thk12378905@gmail.com>
2021-08-16 05:27:32 +0000

    항목 추가 기능

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md'; //컴포넌트처럼 아이콘을 사용할 수 있다.
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+	const[value, setValue] = useState('');
+	
+	const onChagne = useCallback(e => {
+		setValue(e.target.value);
+	}, []);
+	
+	const onSubmit = useCallback(
+	e=> {
+		onInsert(value);
+		setValue(''); //value값 초기화
+		
+		//submit 이벤트는 브라우저에서 새로고침을 발생시킴
+		//이를 방지하기 위해 아래 함수를 호출
+		e.preventDefault();
+	},
+	[onInsert, value],
+	);
+	
 	return (
-		<form className="TodoInsert">
-			<input placeholder="할 일을 입력하세요" />
+		<form className="TodoInsert" onSubmit = {onSubmit}>
+			<input 
+				placeholder="할 일을 입력하세요"
+				value={value}
+				onChange={onChagne}
+				/>
 			<button type="submit">
 				<MdAdd />
 			</button>
