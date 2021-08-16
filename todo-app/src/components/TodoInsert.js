da8ad6bbb741010b82bc91390a58abac841b53c9
김태형 <thk12378905@gmail.com>
2021-08-16 05:37:54 +0000

    삭제기능

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -22,7 +22,7 @@ const TodoInsert = ({ onInsert }) => {
 	);
 	
 	return (
-		<form className="TodoInsert" onSubmit = {onSubmit}>
+		<form className="TodoInsert" onSubmit = {onSubmit} >
 			<input 
 				placeholder="할 일을 입력하세요"
 				value={value}
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -7,7 +7,7 @@ const TodoList = ({ todos }) => {
 	return (
 		<div className="TodoList">
 			{todos.map(todo => (
-				<TodoListItem todo={todo} key={todo.id} />
+				<TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
 			))}
 		</div>
 	);
