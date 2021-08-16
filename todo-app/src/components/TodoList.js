5639021aef6937faf1cf54b8c63b4abc39d510de
김태형 <thk12378905@gmail.com>
2021-08-16 06:41:39 +0000

    수정 기능 추가

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -3,11 +3,16 @@ import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 //여러 종류의 값을 전달해야 하는 경우에는 객체로 통쨰로 전달하는 편이 나중에 성능 최적화를 할 때 편리함
-const TodoList = ({ todos, onRemove }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
 	return (
 		<div className="TodoList">
 			{todos.map(todo => (
-				<TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+				<TodoListItem
+					todo={todo}
+					key={todo.id}
+					onRemove={onRemove}
+					onToggle={onToggle}
+					/>
 			))}
 		</div>
 	);
