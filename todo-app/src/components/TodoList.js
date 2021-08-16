ddb50cadef5b99191499875832abea6280735906
김태형 <thk12378905@gmail.com>
2021-08-16 02:43:26 +0000

    modify UI

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+//여러 종류의 값을 전달해야 하는 경우에는 객체로 통쨰로 전달하는 편이 나중에 성능 최적화를 할 때 편리함
+const TodoList = ({ todos }) => {
 	return (
 		<div className="TodoList">
-			<TodoListItem />
-			<TodoListItem />
-			<TodoListItem />
+			{todos.map(todo => (
+				<TodoListItem todo={todo} key={todo.id} />
+			))}
 		</div>
 	);
 };
