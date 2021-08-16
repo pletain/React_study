81a6b9af2c5d90f7e99c42ebe4cda67a2ae81e4e
김태형 <thk12378905@gmail.com>
2021-08-16 11:25:44 +0000

    listItem 최적화 by react-virtualized

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 //여러 종류의 값을 전달해야 하는 경우에는 객체로 통쨰로 전달하는 편이 나중에 성능 최적화를 할 때 편리함
 const TodoList = ({ todos, onRemove, onToggle }) => {
-	return (
-		<div className="TodoList">
-			{todos.map(todo => (
-				<TodoListItem
-					todo={todo}
-					key={todo.id}
-					onRemove={onRemove}
-					onToggle={onToggle}
-					/>
-			))}
-		</div>
+	const rowRenderer = useCallback(
+	({ index, key, style }) => {
+		const todo = todos[index];
+		return (
+		<TodoListItem
+			todo={todo}
+			key={key}
+			onRemove={onRemove}
+			onToggle={onToggle}
+			style={style}
+			/>
+			);
+	},
+	[onRemove, onToggle, todos],
 	);
+	
+	return (
+		<List
+			className="TodoList"
+			width={512} //전체 크기
+			height={513} //전체 높이
+			rowCount={todos.length} // 항목 개수
+			rowHeight={57}
+			rowRenderer={rowRenderer}
+			list={todos} //배열
+			style={{outline: 'none'}} //List에 기본 적용되는 outline 스타일 제거
+		/>
+		);
 };
 
 export default React.memo(TodoList);
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,22 +7,31 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
 	const { id, text, checked } = todo;
+	
 	return (
-	<div className="TodoListItem">
-	<div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-		{ checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-		<div className="text">{text}</div>
-	</div>
-	<div className="remove" onClick={() => onRemove(id)}>
-	<MdRemoveCircleOutline />
-	</div>
+	<div className="TodoListItem-virtualized" style={style}>
+			<div className="TodoListItem">
+				<div
+					className={cn('checkbox', { checked })}
+					onClick={() => onToggle(id)}
+				>
+					{ checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+					<div className="text">{text}</div>
+				</div>
+				<div className="remove" onClick={() => onRemove(id)}>
+					<MdRemoveCircleOutline />
+				</div>
+			</div>
 	</div>
 	);
 };
 
 //React.memo로 감싸주기만 하면 된다.
 // 이제 TodoListItem 컴포넌트는 todo, onRemove, onToggle이 바뀌지 않으면 리렌더링 하지 X
-export default React.memo(TodoListItem);
+export default React.memo(
+	TodoListItem,
+	(prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
 
