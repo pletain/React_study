db0215f09511d20eb8b3af51cc7d2fedd7ec76cd
김태형 <thk12378905@gmail.com>
2021-08-20 13:40:27 +0000

    Connecting components to specific addresses with router components

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+	<div>
+		<Route path="/" component={Home} />
+		<Route path="/about" component={About} />
+	</div>
+	);
+};
 
 export default App;
