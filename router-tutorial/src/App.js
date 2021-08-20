afb8707cf91e951e679a767e4acc6e66fc7006cb
김태형 <thk12378905@gmail.com>
2021-08-20 13:54:12 +0000

    update prev code

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 const App = () => {
 	return (
 	<div>
-		<Route path="/" component={Home} />
+		<ul>
+			<li>
+			<Link to="/">홈</Link>
+			</li>
+			<li>
+			<Link to="/about">소개</Link>
+			</li>
+		</ul>
+		<Route path="/" component={Home} exact={true}/>
 		<Route path="/about" component={About} />
 	</div>
 	);
