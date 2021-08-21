a70a5fa238ff4c336cea13a528bd870a79c9690b
김태형 <thk12378905@gmail.com>
2021-08-21 03:05:30 +0000

    서브 라우트

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
 	return (
@@ -18,15 +18,13 @@ const App = () => {
 			<Link to="/info">소개2</Link>
 			</li>
 			<li>
-			<Link to="/profile/velopert">velopert 프로필</Link>
-			</li>
-			<li>
-				<Link to="/profile/pletain">pletain 프로필</Link>
+		<Link to="/profiles">프로필</Link>
 			</li>
 		</ul>
+		<hr />
 		<Route path="/" component={Home} exact={true}/>
 		<Route path={['/about', '/info']} component={About} />
-		<Route path="/profile/:username" component={Profile} />
+		<Route path="/profiles" component={Profiles} />
 	</div>
 	);
 };
---
+++ router-tutorial/src/Profiles.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Profile from './Profile';
+
+const Profiles = () => {
+	return (
+	<div>
+		<h3>사용자 목록:</h3>
+		<ul>
+			<li>
+			<Link to="profiles/velopert">velopert</Link>
+			</li>
+			<li>
+			<Link to="/profiles/pletain">pletain</Link>
+			</li>
+			</ul>
+		
+			<Route
+				path="/profiles"
+				exact //JSX에서 props 설정 시 값을 생략하면 자동으로 true
+				render={() => <div>사용자를 선택해 주세요.</div>} //라우터에서 컴포넌트 대신 render라는 props를 전달해줌
+			/>
+			<Route path="/profiles/:username" component={Profile} />
+		</div>
+	);
+};
+
+export default Profiles;
