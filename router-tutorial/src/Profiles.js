279c2abd8a5ab1f49d88d677d0e40844c6bb32a3
김태형 <thk12378905@gmail.com>
2021-08-21 06:18:29 +0000

    NavLink로 선택된 경우 스타일 적용

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 
 const Profiles = () => {
+	const activeStyle = {
+		background: 'black',
+		color: 'white'
+	};
 	return (
 	<div>
 		<h3>사용자 목록:</h3>
 		<ul>
 			<li>
-			<Link to="/profiles/velopert">velopert</Link>
+				<NavLink activeStyle={activeStyle} to="/profiles/velopert">
+					velopert
+				</NavLink>
 			</li>
 			<li>
-			<Link to="/profiles/pletain">pletain</Link>
+				<NavLink activeStyle={activeStyle} to="/profiles/pletain">
+					pletain
+				</NavLink>
 			</li>
 		</ul>
 		
