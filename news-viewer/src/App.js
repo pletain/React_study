5dc0003a4a01937c6197104f06dde76b72b444a4
김태형 <thk12378905@gmail.com>
2021-08-21 11:07:47 +0000

    News Viewer UI

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,27 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 const App = () => {
-	const [data, setData] = useState(null);
-	const onClick = async () => {
-		try {
-			const response = await axios.get(
-			'https://newsapi.org/v2/top-headlines?country=kr&apiKey=4f24aa45e8b34ffdb9e0e062b60bc18d',
-			);
-			setData(response.data);
-		} catch (e) {
-			console.log(e);
-		}
-	};
-	
-	return (
-		<div>
-			<div>
-				<button onClick={onClick}>불러오기</button>
-			</div>
-			{data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-		</div>
-	);
+	return <NewsList />;
 };
 
 export default App;
