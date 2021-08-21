5d186cae7adc2205e0677d0281e36406ed26e29c
김태형 <thk12378905@gmail.com>
2021-08-21 06:13:41 +0000

    Switch로 Not Found 페이지 구현

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
@@ -26,11 +26,21 @@ const App = () => {
 			</li>
 		</ul>
 		<hr />
-			
-		<Route path="/" component={Home} exact={true}/>
-		<Route path={['/about', '/info']} component={About} />
-		<Route path="/profiles" component={Profiles} />
-		<Route path="/history" component={HistorySample} />
+		<Switch>
+			<Route path="/" component={Home} exact={true}/>
+			<Route path={['/about', '/info']} component={About} />
+			<Route path="/profiles" component={Profiles} />
+			<Route path="/history" component={HistorySample} />
+			<Route
+				//path를 따로 정의하지 않으면 모든 상황에 렌더링됨
+				render={({ location }) => (
+				<div>
+					<h2>이 페이지는 존재하지 않습니다:</h2>
+					<p>{location.pathname}</p>
+				</div>
+				)}
+			/>
+		</Switch>
 	</div>
 	);
 };
