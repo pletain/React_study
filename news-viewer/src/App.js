f5f647e7b2a4d7a2392451198c26a6264427cdfc
김태형 <thk12378905@gmail.com>
2021-08-21 07:06:53 +0000

    async/await

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-cosnt App = () => {
+const App = () => {
 	const [data, setData] = useState(null);
-	const onClick = () => {
-		axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+	const onClick = async () => {
+		try {
+			const response = await axios.get(
+			'https://jsonplaceholder.typicode.com/todos/1',
+			);
 			setData(response.data);
-		});
+		} catch (e) {
+			console.log(e);
+		}
 	};
 	return (
 		<div>
