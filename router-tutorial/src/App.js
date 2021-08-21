60843443e43ef3f34291da35b3da19e50cc43704
김태형 <thk12378905@gmail.com>
2021-08-21 06:06:32 +0000

    withRouter

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -26,6 +26,7 @@ const App = () => {
 			</li>
 		</ul>
 		<hr />
+			
 		<Route path="/" component={Home} exact={true}/>
 		<Route path={['/about', '/info']} component={About} />
 		<Route path="/profiles" component={Profiles} />
--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -8,20 +8,22 @@ const Profiles = () => {
 		<h3>사용자 목록:</h3>
 		<ul>
 			<li>
-			<Link to="profiles/velopert">velopert</Link>
+			<Link to="/profiles/velopert">velopert</Link>
 			</li>
 			<li>
 			<Link to="/profiles/pletain">pletain</Link>
 			</li>
-			</ul>
+		</ul>
 		
 			<Route
 				path="/profiles"
 				exact //JSX에서 props 설정 시 값을 생략하면 자동으로 true
 				render={() => <div>사용자를 선택해 주세요.</div>} //라우터에서 컴포넌트 대신 render라는 props를 전달해줌
 			/>
+			
 			<Route path="/profiles/:username" component={Profile} />
 		</div>
+		
 	);
 };
 
