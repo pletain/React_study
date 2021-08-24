0e6c28f3156744d3545737bd5734621e815938a6
김태형 <thk12378905@gmail.com>
2021-08-24 11:02:46 +0000

    static contextType 사용하기

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,32 +1,44 @@
-import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import React, { Component } from 'react';
+import ColorContext from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const SelectColors = () => {
-	return (
-	<div>
-		<h2>색상을 선택하세요.</h2>
-		<ColorConsumer>
-			{({ actions }) => (
-			<div style={{ display: 'flex' }}>
+class SelectColors extends Component {
+	static contextType = ColorContext;
+
+	handleSetColor = color => {
+		this.context.actions.setColor(color);
+	};
+
+	handleSetSubColor = subcolor => {
+		this.context.actions.setSubcolor(subcolor);
+	};
+
+	render() {
+		return (
+			<div>
+				<h2>색상을 선택하세요.</h2>
+				<div style={{ display: 'flex' }}>
 					{colors.map(color => (
 						<div
-						key={color}
-						style={{background: color, width: '24px', height: '24px', cursor: 'pointer' }}
-							onClick={() => actions.setColor(color)}
+							key={color}
+							style={{background: color,
+								width: '24px',
+								height: '24px',
+								cursor: 'pointer'
+							}}
+							onClick={() => this.handleSetColor(color)}
 							onContextMenu={e => {
-								e.preventDefault(); //마우스 오른 쪽 버튼 클릭 시 메뉴가 뜨는 것을 무시함
-								actions.setSubcolor(color);
+								e.preventDefault();
+								this.handleSetSubcolor(color);
 							}}
 							/>
 						))}
 					</div>
-				)}
-			</ColorConsumer>
-			<hr />
-		</div>
-	);
-};
+				<hr />
+			</div>
+		);
+		}
+	}
 
 export default SelectColors;
