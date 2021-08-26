86793ce60e35369a1ebdbfc291e93957a9cf5fcd
김태형 <thk12378905@gmail.com>
2021-08-26 13:59:03 +0000

    define reducer	ftn

--- vanila-redux/index.js
+++ vanila-redux/index.js
@@ -15,3 +15,36 @@ const initialState = {
 	toggle: false,
 	counter: 0
 };
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+	toggle: false,
+	counter: 0
+};
+
+//state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+	//action.type에 따라 다른 작업을 처리함
+	switch(action.type) {
+		case TOGGLE_SWITCH:
+			return {
+				...state, //불변성 유지를 해 주어야 합니다.
+				toggle: !.state.toggle
+			};
+		case INCREASE:
+			return {
+				...state,
+				counter: state.counter + action.difference
+			};
+		case DECREASE:
+			return {
+				...state,
+				counter: state.counter - 1
+			};
+		default: 
+			return state;
+	}
+}
