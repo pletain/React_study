fa5dce4ce4bbfca1e284bbd094e0ac8089531ddb
김태형 <thk12378905@gmail.com>
2021-08-26 13:39:45 +0000

    DOM 레퍼런스 만들기

--- vanila-redux/index.js
+++ vanila-redux/index.js
@@ -1,1 +1,17 @@
-colsole.log('hello parcel');
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelectro('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => {{ type: TOGGLE_SWITCH }};
+const increase = difference => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+	toggle: false,
+	counter: 0
+};
