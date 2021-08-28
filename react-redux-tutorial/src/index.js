29c0fd4a837e7eee56da346e7b023c6824bad466
김태형 <thk12378905@gmail.com>
2021-08-28 05:49:21 +0000

    modify

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux' //리액트 컴포넌트에서 스토어를 사용할 수 있도록 App 컴포넌트를 감싸줌
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 
 ReactDOM.render(
-	<Provider store={sotre}>
+	<Provider store={store}>
 	<App />
 	</Provider>,
 	document.getElementByID('root'),
--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -47,7 +47,7 @@ const initialState = {
 };
 
 function todos(state = initialState, action) {
-	switch (action,type) {
+	switch (action.type) {
 		case CHANGE_INPUT:
 			return {
 				...state,
