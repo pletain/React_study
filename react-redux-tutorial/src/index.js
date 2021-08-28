9ebc4e579b23f80d7c5a8e7e8e50eb70a6cc0332
김태형 <thk12378905@gmail.com>
2021-08-28 05:52:23 +0000

    modify-well

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux' //리액트 컴포넌트에서 스토어를 사용할 수 있도록 App 컴포넌트를 감싸줌
+import { Provider } from 'react-redux'; //리액트 컴포넌트에서 스토어를 사용할 수 있도록 App 컴포넌트를 감싸줌
 import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
@@ -14,5 +14,5 @@ ReactDOM.render(
 	<Provider store={store}>
 	<App />
 	</Provider>,
-	document.getElementByID('root'),
+	document.getElementById('root'),
 );
