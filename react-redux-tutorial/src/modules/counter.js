1fb40a1b85934b0ece848c2677df5ec2dadcfc17
김태형 <thk12378905@gmail.com>
2021-08-28 11:12:58 +0000

    redux-actions intro

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,28 +1,28 @@
+import { createAction } from 'redux-actions';
+
 //액션 타입 정의하기
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 //액션 생성 함수 만들기
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+//createAction을 사용하면 매번 객체를 직접 만들어주지 않아도 됨.
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //초기 상태 및 리듀서 함수 만들기
 const initialState = {
 	number: 0
 }
+//handleActions로 리듀서 함수 간단하게 작성
+//첫 번째 파라미터에는 각 액션에 대한 업데이트 함수를 넣고
+//두 번째 파라미터에는 초기 상태를 넣어줌 
+const counter = handleActions(
+{
+	[INCREASE]: (state, action) => ({ number: state.number + 1}),
+	[DECREASE]: (state, action) => ({ number: state.number - 1}),
+},
+initialState,
+)
 
-function counter(state = initialState, action) {
-	switch (action.type) {
-		case INCREASE:
-			return {
-				number: state.number + 1
-			}
-		case DECREASE:
-			return {
-				number: state.number - 1
-			};
-		default:
-			return state;
-	}
-}
+
 
 export default counter;
--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,33 +1,23 @@
-//액션 타입 정의학
+import { createAction } from 'redux-actions';
+
+//액션 타입 정의화
 const CHANGE_INPUT = 'todos/CHNAGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
 const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
 const REMOVE = 'todos/REMOVE'; // todo를 제거함
 
 //액션 생성 함수 만들기
-export const changeInput = input => ({
-	type: CHANGE_INPUT,
-	input
-});
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+
 let id = 3; //insert가 호출될 떄마다 1씩 더해짐
-export const insert = text => ({
-	type: INSERT,
-	todo: {
-		id: id++,
-		text,
-		done: false
-	}
-});
+export const insert = createAction(INSERT, text => ({
+	id: id++,
+	text,
+	done: false,
+}));
 
-export const toggle = id => ({
-	type: TOGGLE,
-	id
-});
-
-export const remove = id => ({
-	type: REMOVE,
-	id
-});
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
 //초기 상태 및 리듀서 함수 만들기
 const initialState = {
