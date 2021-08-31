fb571791137116a2a1dbcffcb4b539deb466640a
김태형 <thk12378905@gmail.com>
2021-08-31 14:57:14 +0000

    loadable components_preload

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, Suspense } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import loadable from '@loadable/component';
-const SpliteMe = loadable(() => import('SpliteMe'). {
+const SplitMe = loadable(() => import('./SplitMe'), {
 	fallback: <div>loading...</div>
 });
 
@@ -11,15 +11,19 @@ function App() {
 	const onClick = () => {
 		setVisible(true);
 	};
+	const onMouseOver = () => {
+		SplitMe.preload();
+	};
 	return (
 	<div className="App">
 		<header className="App-header">
 			<img src={logo} className="App-logo" alt="logo" />
-			<p onClick={onClick}>Hello React!</p>
-			{visible && <SpliteMe />}
+			<p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p> 
+			{visible && <SplitMe />}
 			</header>
 		</div>
 		);
 	}
-
+//Hello React! 위에 마우스 커서를 올리기만 해도 로딩이 시작됨.(커서 올리는 시점에서 파일이 불어와짐)
+//클릭했을 때 렌더링 됨.
 export default App;
